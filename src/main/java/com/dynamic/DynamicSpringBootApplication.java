bb0465b8031e76c4e766caa4eb10c21c786279a9
Bruno <brunomartins1030@gmail.com>
2024-12-15 13:24:44 +0000

    first commit

---
+++ src/main/java/com/dynamic/DynamicSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.dynamic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DynamicSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DynamicSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dynamic/DynamicSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dynamic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DynamicSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
