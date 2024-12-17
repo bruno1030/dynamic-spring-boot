498be957b41353a2346aa1e4bc0409c65045f0af
Bruno <brunomartins1030@gmail.com>
2024-12-17 22:31:21 +0000

    add new classes

---
+++ src/main/java/com/dynamic/dto/ProductDto.java
@@ -0,0 +1,64 @@
+package com.dynamic.dto;
+
+public class ProductDto {
+
+    private Integer id;
+
+    private String description;
+
+    private Double value;
+
+    private Integer stockQuantity;
+
+    private String title;
+
+    private String brand;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Double getValue() {
+        return value;
+    }
+
+    public void setValue(Double value) {
+        this.value = value;
+    }
+
+    public Integer getStockQuantity() {
+        return stockQuantity;
+    }
+
+    public void setStockQuantity(Integer stockQuantity) {
+        this.stockQuantity = stockQuantity;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+}
