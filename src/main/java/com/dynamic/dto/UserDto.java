1c5af514c3929073c313ad6ac625432a12f61667
Bruno <brunomartins1030@gmail.com>
2025-03-01 20:32:43 +0000

    Add user repository

--- src/main/java/com/dynamic/dto/UserDto.java
+++ src/main/java/com/dynamic/dto/UserDto.java
@@ -1,11 +1,7 @@
 package com.dynamic.dto;
 
-import com.dynamic.entity.Role;
 import lombok.Getter;
 import lombok.Setter;
-import org.springframework.data.annotation.Id;
-import org.springframework.data.relational.core.mapping.Column;
-import org.springframework.data.relational.core.mapping.MappedCollection;
 
 @Getter
 @Setter
